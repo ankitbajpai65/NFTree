61121eda5cec81088fbaa86a905cb70bcd7bc77b
srivastavatanuj <tanuj.srivastava06@gmail.com>
2023-11-09 11:22:38 +0530

    nav from main to master

---
+++ src/Components/Navbar/Dropdown.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import { List, ListItem } from "@mui/material";
+import { Link } from "react-router-dom";
+
+const projectDropdown = [
+  { to: '/createProject', name: 'Create Project' },
+  { to: '/ongoingProjects', name: 'Ongoing Project' },
+  { to: '/completedProjects', name: 'Completed Project' },
+]
+
+const profileDropdown = [
+  { to: '/kyc', name: 'Dashboard' },
+  { to: '/kyc', name: 'Edit Profile' },
+  { to: '/kyc', name: 'Complete KYC' },
+  { to: '/kyc', name: 'Logout' },
+]
+
+const Dropdown = ({ handleMouseLeave, setMobileOpen, dropdown }) => {
+  console.log({ handleMouseLeave, setMobileOpen, dropdown });
+  return (
+    <>
+      <List
+        sx={{
+          color: "black",
+          padding: 0,
+          backgroundColor: "white",
+          position: "absolute",
+          top: dropdown === "Projects" ? { xs: "2rem", sm: "3rem" } : '2.5rem',
+          left: dropdown === "Projects" ? { xs: "-65px", sm: "unset" } : '-5rem',
+          width: dropdown === "Projects" ? { xs: "12rem", sm: "auto" } : '8rem',
+          zIndex: "100",
+          boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+          borderRadius: "4px",
+        }}
+      >
+        <ListItem disablePadding>
+          <div style={{ margin: "auto" }}>
+            {
+              dropdown === 'Projects' ?
+                (
+                  projectDropdown.map((elem, index) => {
+                    return (
+                      <Link
+                        key={index}
+                        to={elem.to}
+                        onClick={() => {
+                          handleMouseLeave();
+                          setMobileOpen(false);
+                        }}
+                        className="dropdownLinks"
+                      >
+                        {elem.name}
+                      </Link>
+
+                    )
+                  })
+                )
+                :
+                (
+                  profileDropdown.map((elem, index) => {
+                    return (
+                      <Link
+                        key={index}
+                        to={elem.to}
+                        onClick={() => {
+                          handleMouseLeave();
+                          setMobileOpen(false);
+                        }}
+                        className="dropdownLinks"
+                      >
+                        {elem.name}
+                      </Link>
+                    )
+                  })
+                )
+            }
+          </div>
+        </ListItem>
+      </List>
+    </>
+  );
+};
+
+export default Dropdown;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,123 +1,332 @@
 import React, { useState } from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route, useNavigate, Link } from "react-router-dom";
 import Home from "../Home/Home";
+import Kyc from "../Kyc/Kyc";
+import Dropdown from "./Dropdown";
+import Signin from "../Auth/Signin";
+import Signup from "../Auth/Signup";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import CreateProject from "../Projects/CreateProject/CreateProject";
-import Auth from "../Auth/Auth";
-import { useAuth } from "../../Contexts/AuthContext";
-import { googleLogout } from "@react-oauth/google";
+import OngoingProjects from "../Projects/OngoingProjects";
+import ProjectDetails from "../Projects/ProjectDetails";
+import ContributeProject from "../Projects/ContributeProject/ContributeProject";
+import CompletedProjects from "../Projects/CompletedProjects";
+
+import {
+  AppBar,
+  Box,
+  Grid,
+  CssBaseline,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  Toolbar,
+  Typography,
+  Button,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 import logo1 from "/logo_colored.png";
-import OngoingProject from "../Projects/OngoingProject/OngoingProject";
 import "../Navbar/Navbar.css";
-import Contact from "../Contact_us/Contact";
-import ErrorPage from "../Error_page/ErrorPage";
-import ProfilePage from "../UserProfile/ProfilePage";
+
+const drawerWidth = 240;
+const navItems = ["Home", "Projects", "Contact"];
 
 export default function Navbar() {
   const navigate = useNavigate();
-  const { authUser, setAuthUser, isLoggedIn, setIsLoggedIn } = useAuth();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [scroll, setScroll] = useState(false);
+  const [isMouseEnter, setIsMouseEnter] = useState(false);
+  const [isMouseEnterProfile, setIsMouseEnterProfile] = useState(false);
+  const [isLogin, setIsLogin] = useState(false);
 
-  console.log(isLoggedIn);
+  const scrolling = () => {
+    window.scrollY >= 2 ? setScroll(true) : setScroll(false);
+  };
+  window.addEventListener("scroll", scrolling);
 
-  function googleLogoutBtn() {
-    googleLogout();
-    setAuthUser("");
-    setIsLoggedIn(false);
-    navigate("/");
-  }
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+    if (isMouseEnter) setIsMouseEnter(false);
+  };
+
+  const handleMouseEnter = () => setIsMouseEnter(true);
+
+  const handleMouseLeave = () => {
+    setIsMouseEnter(false);
+    setMobileOpen(false);
+  };
+
+  const handleMouseEnterProfile = () => setIsMouseEnterProfile(true);
+
+  const handleMouseLeaveProfile = () => {
+    setIsMouseEnterProfile(false);
+    setMobileOpen(false);
+  };
+
+  const handleSidebarClick = (event) => {
+    const clickedElement = event.target;
+    if (!clickedElement.classList.contains("projectLink")) {
+      setIsMouseEnter(false);
+    }
+  };
+
+  const drawer = (
+    <Box
+      sx={{
+        textAlign: "center",
+        zIndex: "20",
+      }}
+      id="sidebar"
+    >
+      <Typography variant="h6" sx={{ my: 2 }}>
+        <img
+          src={logo1}
+          alt=""
+          onClick={() => navigate('/')}
+          style={{
+            height: "3.5rem",
+            marginTop: ".5rem",
+          }}
+        />
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item, id) => (
+          <ListItem key={id} disablePadding>
+            <div
+              style={{
+                padding: "10px",
+                margin: "auto",
+              }}
+            >
+              {item === "Projects" ? (
+                <>
+                  <div
+                    style={{ position: "relative" }}
+                    className="solutionDivSidebar"
+                  >
+                    <p
+                      className="navbarLinks projectLink"
+                      onClick={handleMouseEnter}
+                      style={{ color: "black" }}
+                    // onClick={()=>navigate('/')}
+                    >
+                      {item}
+                    </p>
+                    {isMouseEnter && (
+                      <Dropdown
+                        handleMouseLeave={handleMouseLeave}
+                        setMobileOpen={setMobileOpen}
+                      />
+                    )}
+                  </div>
+                </>
+              ) : (
+                <Link
+                  to={`/${item.toLowerCase()}`}
+                  className="navbarLinks otherLinks"
+                  onClick={handleDrawerToggle}
+                  style={{ color: "black" }}
+                >
+                  {item}
+                </Link>
+              )}
+            </div>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
 
   return (
     <>
-      <div style={{ height: "0px" }}>
-        <nav>
-          <div id="navbarLogo">
-            <img src={logo1} onClick={() => navigate("/")} alt="" />
-          </div>
-          <div>
-            <ul className="navItems">
-              <li>
-                <h4 className="active" onClick={() => navigate("/")}>
-                  Home
-                </h4>
-              </li>
-              <li className="submenus">
-                <h4>Projects</h4>
-                <ul>
-                  {isLoggedIn && (
-                    <li onClick={() => navigate("/create-project")}>
-                      <p>Create Project</p>
-                    </li>
-                  )}
-                  <li onClick={() => navigate("/ongoing-project")}>
-                    <p>Ongoing Projects</p>
-                  </li>
-                  <li onClick={() => navigate("/complete-project")}>
-                    <p>Completed Projects</p>
-                  </li>
-                </ul>
-              </li>
-              <li onClick={() => navigate("/contact-us")}>
-                <h4>Contact us</h4>
-              </li>
-              <div className="responsiveBtn">
-                <li style={{ padding: "0" }} className="submenus">
-                  {!isLoggedIn ? (
-                    <button
-                      className="loginBtn"
-                      style={{ backgroundColor: "red" }}
-                      onClick={() => navigate("/auth")}
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar
+          component="nav"
+          sx={{
+            // border: '2px solid red',
+            zIndex: 10,
+            boxShadow: "none",
+            background: "var(--green-10)",
+            position: "fixed",
+            top: 0,
+          }}
+        >
+          <Toolbar>
+            <MenuIcon
+              onClick={handleDrawerToggle}
+              sx={{
+                display: { sm: "none" },
+                filter: "invert(1)"
+              }}
+            />
+            <Grid
+              container
+              sx={{
+                // border: '2px solid red',
+                width: "100%",
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <Grid
+                item
+                sm={4}
+                md={5}
+                component="div"
+                sx={{
+                  // border: '2px solid blue',
+                  display: { xs: "none", sm: "block" },
+                  textAlign: "start",
+                  pl: { sm: 0, md: 5 },
+                }}
+                className="navbarTitle"
+              >
+                <img
+                  src={logo1}
+                  onClick={() => navigate('/')}
+                  alt=""
+                  style={{
+                    height: "3rem",
+                    margin: "auto",
+                    marginTop: ".5rem",
+                    cursor: 'pointer'
+                  }}
+                  id="navbarLogo"
+                />
+              </Grid>
+              <Grid
+                item
+                className="navLinksContainer"
+                sm={8}
+                md={7}
+                sx={{
+                  // border: '2px solid red',
+                  display: { xs: "none", sm: "flex" },
+                  width: { sm: "70%", md: "50%" },
+                }}
+              >
+                <Box
+                  sx={{
+                    width: "100%",
+                    display: "flex",
+                    justifyContent: "flex-start",
+                    alignItems: "center",
+                  }}
+                >
+                  {navItems.map((item, id) => (
+                    <div
+                      key={id}
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                      }}
                     >
-                      login
-                    </button>
-                  ) : (
-                    <button
-                      className="loginBtn"
-                      onClick={() => navigate("/profile")}
-                      style={{ backgroundColor: "red" }}
-                    >
-                      {authUser["name"].split(" ")[0]}
-                    </button>
-                  )}
-                  {isLoggedIn && (
-                    <ul
-                      style={{ right: "0px", marginTop: "2px", width: "100px" }}
-                    >
-                      <li
-                        onClick={() => {
-                          googleLogoutBtn();
-                        }}
-                      >
-                        <p>Logout</p>
-                      </li>
-                    </ul>
-                  )}
-                </li>
-              </div>
-            </ul>
-          </div>
-          <div className="menu">
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-        </nav>
-      </div>
+                      {item === "Projects" ? (
+                        <>
+                          <div
+                            onMouseOver={handleMouseEnter}
+                            onMouseOut={handleMouseLeave}
+                            style={{
+                              height: "2rem",
+                              display: "flex",
+                              alignItems: "center",
+                            }}
+                          >
+                            <a className="projectLink">{item}</a>
+                            {isMouseEnter && (
+                              <Dropdown
+                                handleMouseLeave={handleMouseLeave}
+                                setMobileOpen={setMobileOpen}
+                                dropdown={"Projects"}
+                              />
+                            )}
+                          </div>
+                        </>
+                      ) : (
+                        <Link
+                          to={`/${item.toLowerCase()}`}
+                          className="navbarLinks"
+                        >
+                          {item}
+                        </Link>
+                      )}
+                    </div>
+                  ))}
+                </Box>
+              </Grid>
+            </Grid>
+            {
+              !isLogin ?
+                <Button
+                  variant="contained"
+                  className="filledBtn"
+                  onClick={() => navigate("/signin")}
+                >
+                  Login
+                </Button>
+                :
+                <>
+                  <div
+                    style={{ position: 'relative' }}
+                    onMouseOver={handleMouseEnterProfile}
+                    onMouseOut={handleMouseLeaveProfile}
+                  >
+                    <AccountCircleIcon sx={{
+                      color: 'black',
+                      fontSize: '2.5rem'
+                    }} />
+                    {isMouseEnterProfile && (
+                      <Dropdown
+                        handleMouseLeave={handleMouseLeaveProfile}
+                        setMobileOpen={setMobileOpen}
+                        dropdown={"Profile"}
+                      />
+                    )}
+                  </div>
+                </>
+            }
+          </Toolbar>
+        </AppBar>
+        <Box component="nav">
+          <Drawer
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{
+              keepMounted: true,
+            }}
+            onClick={handleSidebarClick}
+            sx={{
+              display: { xs: "block", sm: "none" },
+              "& .MuiDrawer-paper": {
+                boxSizing: "border-box",
+                width: drawerWidth,
+              },
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Box>
+      </Box>
 
       <Routes>
         <Route index element={<Home />} />
-        <Route path="/" element={<Home />} />
-        <Route path="auth" element={<Auth />} />
-        <Route path="ongoing-project" element={<OngoingProject />} />
-        <Route path="contact-us" element={<Contact />} />
-        <Route path="profile" element={<ProfilePage data={authUser} />} />
-        {isLoggedIn && (
-          <>
-            {/* <Route path="kyc" element={<Kyc />} />
-            <Route path="create-project" element={<CreateProject />} />
-            <Route path="profile" element={<EditProfile data={authUser} />} /> */}
-          </>
-        )}
-
-        <Route path="*" element={<ErrorPage />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/kyc" element={<Kyc />} />
+        <Route path="/createProject" element={<CreateProject />} />
+        <Route path="/ongoingProjects" element={<OngoingProjects />} />
+        <Route path="/ongoingProjects/projectName" element={<ProjectDetails progress="10/100"/>} />
+        <Route path="/ongoingProjects/projectName/contribute" element={<ContributeProject/>} />
+        <Route path="/completedProjects" element={<CompletedProjects />} />
+        <Route path="/completedProjects/projectName" element={<ProjectDetails progress="100/100"/>} />
+        {/* <Route path="*" element={<NoPage />} /> */}
       </Routes>
     </>
   );
