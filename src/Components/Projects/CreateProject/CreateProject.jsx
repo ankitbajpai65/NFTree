392ce36ae5e6626195062e757dfb03b2d3a4c6a6
Ankit Bajpai <ankit65bajpai@gmail.com>
2023-11-16 12:15:26 +0530

    Added validation in create project and kyc

--- src/Components/Projects/CreateProject/CreateProject.jsx
+++ src/Components/Projects/CreateProject/CreateProject.jsx
@@ -1,39 +1,61 @@
+import React, { useCallback } from 'react';
 import "./CreateProject.css";
 import { useFormik } from "formik";
 import { createProjectSchema } from "./ValidationSchema";
 import { createProject } from "../../../api/projectApi";
-import { useState } from "react";
-
-const initialValues = {
-  name: "",
-  description: "",
-  plant_types: "",
-  area: "",
-  plant_planned: "",
-  plants_planted: 0,
-  donation: "",
-  address: "",
-  city: "",
-  country: "",
-  document: "",
-  image: "",
-  is_completed: false,
-  user: "",
-};
 
 const CreateProject = () => {
-  const [showError, setShowError] = useState(false);
-  const { values, errors, handleChange, handleSubmit, touched, setFieldValue } =
-    useFormik({
-      initialValues: initialValues,
-      validationSchema: createProjectSchema,
-      onSubmit: (values, action) => {
-        // action.resetForm();
-        values.user = sessionStorage.getItem("id");
-        console.log(values);
-        createProject(values);
-      },
-    });
+  // const [showError, setShowError] = useState(false);
+  // const { values, errors, handleChange, handleSubmit, touched, setFieldValue } =
+  //   useFormik({
+  //     initialValues: initialValues,
+  //     validationSchema: createProjectSchema,
+  //     onSubmit: (values, action) => {
+  //       // action.resetForm();
+  //       values.user = sessionStorage.getItem("id");
+  //       console.log(values);
+  //       createProject(values);
+  //     },
+  //   });
+
+  const handleOnSubmit = (values) => {
+    const fullName = Object.keys(values)
+      .map((key) => values[key])
+      .join(" ");
+    alert(`Hello ${fullName}!`);
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      projectName: "",
+      description: "",
+      plantType: "",
+      area: "",
+      totalPlantsPlanned: "",
+      // plants_planted: 0,
+      donation: "",
+      address: "",
+      city: "",
+      country: "",
+      document: "",
+      image: "",
+      is_completed: false,
+      user: "",
+    },
+    validationSchema: createProjectSchema,
+    onSubmit: handleOnSubmit,
+  });
+
+  const setInputValue = useCallback(
+    (key, value) =>
+      formik.setValues({
+        ...formik.values,
+        [key]: value,
+      }),
+    [formik]
+  );
+
+  console.log(formik);
 
   return (
     <>
@@ -45,198 +67,135 @@ const CreateProject = () => {
           <h2 className="formHead">
             Let's Plant Trees: Nurturing the Future, One Sapling at a Time
           </h2>
-          <form className="form-input" onSubmit={handleSubmit}>
+
+          <form className="form-input" onSubmit={formik.handleSubmit}>
             <input
               type="text"
-              name="name"
+              name="projectName"
               placeholder="Project name"
-              value={values.name}
-              onChange={handleChange}
+              value={formik.values.projectName}
+              onChange={(e) => setInputValue("projectName", e.target.value)}
             />
+            <small>{formik.errors.projectName}</small>
 
             <textarea
               name="description"
               cols="30"
               rows="5"
               placeholder="Tell us a little about project (100 Character atleast)"
-              value={values.description}
-              onChange={handleChange}
+              value={formik.values.description}
+              onChange={(e) => setInputValue("description", e.target.value)}
             />
+            <small>{formik.errors.description}</small>
 
-            <br></br>
             <input
               type="text"
-              name="plant_types"
-              placeholder="Types of Plants (use comma to seperate)"
-              value={values.plant_types}
-              onChange={handleChange}
+              name="plantType"
+              placeholder="Type of Plants (use comma to seperate)"
+              value={formik.values.plantType}
+              onChange={(e) => setInputValue("plantType", e.target.value)}
             />
+            <small>{formik.errors.plantType}</small>
 
             <input
               type="text"
               name="area"
               placeholder="Total Plantation Area in Square Meter"
-              onInput={(e) =>
-                (e.target.value = e.target.value.replace(/[^0-9 ]/g, ""))
-              }
-              value={values.area}
-              onChange={handleChange}
+              value={formik.values.area}
+              onChange={(e) => setInputValue("area", e.target.value)}
             />
+            <small>{formik.errors.area}</small>
 
             <div className="form-col">
-              <input
-                type="text"
-                name="plant_planned"
-                placeholder="No of Plants Planned"
-                onInput={(e) =>
-                  (e.target.value = e.target.value.replace(/[^0-9 ]/g, ""))
-                }
-                value={values.plant_planned}
-                onChange={handleChange}
-              />
+              <div>
+                <input
+                  type="text"
+                  name="totalPlantsPlanned"
+                  placeholder="No of Plants Planned"
+                  value={formik.values.totalPlantsPlanned}
+                  onChange={(e) => setInputValue("totalPlantsPlanned", e.target.value)}
+                />
+                <small>{formik.errors.totalPlantsPlanned}</small>
+              </div>
 
-              <input
-                type="text"
-                name="donation"
-                placeholder="Donation per Plant"
-                onInput={(e) =>
-                  (e.target.value = e.target.value.replace(/[^0-9 ]/g, ""))
-                }
-                value={values.donation}
-                onChange={handleChange}
-              />
+              <div>
+                <input
+                  type="text"
+                  name="donation"
+                  placeholder="Donation per Plant"
+                  value={formik.values.donation}
+                  onChange={(e) => setInputValue("donation", e.target.value)}
+                />
+                <small>{formik.errors.donation}</small>
+              </div>
             </div>
-
-            <br></br>
 
             <input
               type="text"
               name="address"
               placeholder="Address"
-              value={values.address}
-              onChange={handleChange}
+              value={formik.values.address}
+              onChange={(e) => setInputValue("address", e.target.value)}
             />
+            <small>{formik.errors.address}</small>
 
             <div className="form-col">
-              <input
-                type="text"
-                name="city"
-                placeholder="City"
-                value={values.city}
-                onChange={handleChange}
-              />
+              <div>
+                <input
+                  type="text"
+                  name="city"
+                  placeholder="City"
+                  value={formik.values.city}
+                  onChange={(e) => setInputValue("city", e.target.value)}
+                />
+                <small>{formik.errors.city}</small>
+              </div>
 
-              <input
-                type="text"
-                name="country"
-                placeholder="Country"
-                value={values.country}
-                onChange={handleChange}
-              />
+              <div>
+                <input
+                  type="text"
+                  name="country"
+                  placeholder="Country"
+                  value={formik.values.country}
+                  onChange={(e) => setInputValue("country", e.target.value)}
+                />
+                <small>{formik.errors.country}</small>
+              </div>
             </div>
-            <br></br>
-            <div className="projectFile">
-              <input
-                id="inputTag"
-                type="file"
-                name="document"
-                accept=".zip,.pdf"
-                onChange={(event) => {
-                  setFieldValue("document", event.currentTarget.files[0]);
-                }}
-                className={values.document ? "" : "projectDoc"}
-              />
 
-              <br />
-              <br />
-              <input
-                id="inputTag"
-                type="file"
-                name="image"
-                accept="image/*"
-                onChange={(event) => {
-                  setFieldValue("image", event.currentTarget.files[0]);
-                }}
-                className={values.image ? "" : "projectImg"}
-              />
-            </div>
+            {/* <div className="projectFile"> */}
+            <input
+              id="inputTag"
+              type="file"
+              name="document"
+              accept=".zip,.pdf"
+              placeholder="Upload documents"
+              value={formik.values.document}
+              onChange={(e) => setInputValue("document", e.target.value)}
+            />
+            <small>{formik.errors.document}</small>
+
+            <input
+              id="inputTag"
+              type="file"
+              name="image"
+              accept="image/*"
+              value={formik.values.image}
+              onChange={(e) => setInputValue("image", e.target.value)}
+            />
+            <small>{formik.errors.image}</small>
+            {/* </div> */}
+
+            {/* {!!formik.errors.lastName && <br />} */}
+
             <div className="form-button">
-              <button type="submit" className="submit-button">
+              <button type="submit" disabled={!formik.isValid} className="submit-button">
                 Get Started
               </button>
             </div>
           </form>
         </div>
-      </div>
-      {Object.keys(errors).length != 0 && (
-        <div className="errorBox">
-          <h3>Errors</h3>
-          {errors.name && touched.name ? (
-            <span className="form-error">{errors.name}</span>
-          ) : (
-            ""
-          )}
-
-          {errors.description && touched.description ? (
-            <span className="form-error">{errors.description}</span>
-          ) : (
-            ""
-          )}
-
-          {errors.plant_types && touched.plant_types ? (
-            <span className="form-error">{errors.plant_types}</span>
-          ) : (
-            ""
-          )}
-
-          {errors.area && touched.area ? (
-            <span className="form-error">{errors.area}</span>
-          ) : (
-            ""
-          )}
-
-          {errors.plant_planned && touched.plant_planned ? (
-            <span className="form-error">{errors.plant_planned}</span>
-          ) : (
-            ""
-          )}
-          {errors.donation && touched.donation ? (
-            <span className="form-error">{errors.donation}</span>
-          ) : (
-            ""
-          )}
-
-          {errors.address && touched.address ? (
-            <span className="form-error">{errors.address}</span>
-          ) : (
-            ""
-          )}
-
-          {errors.city && touched.city ? (
-            <span className="form-error">{errors.city}</span>
-          ) : (
-            ""
-          )}
-
-          {errors.country && touched.country ? (
-            <span className="form-error">{errors.country}</span>
-          ) : (
-            ""
-          )}
-
-          {errors.document && touched.document ? (
-            <span className="form-error">{errors.document}</span>
-          ) : (
-            ""
-          )}
-
-          {errors.image && touched.image ? (
-            <span className="form-error">{errors.image}</span>
-          ) : (
-            ""
-          )}
-        </div>
-      )}
+      </div >
     </>
   );
 };
